f0c3fed9980e144fd4998845b77221cd53e592ad
Monkman <Martin.Monkman@gov.bc.ca>
2019-10-15 07:33:15 -0700

    merge

---
+++ class_1/ho_1_01.R
@@ -0,0 +1,37 @@
+## CLASS 1
+#
+### Example 1
+#
+# R4DS, ch.2
+#
+#### Steps
+#
+#1. Open RStudio
+#
+#
+# > panes
+#
+# R as calculator -- Console pane
+10 + 4
+# Creating a named object with assignment arrow `<-`
+x <- 10
+y <- 4
+x + y
+
+# > Did you notice the change in the Environment pane?
+
+# How would you set `x` equal to 20?
+
+x <- 20
+
+# And now divide `x` by `y`
+
+x / y
+
+### R files
+
+# * R
+
+# * R Markdown
+
+
---
+++ resources/datasources.R
@@ -0,0 +1,39 @@
+# Data sources
+
+## R for Data Science
+
+### {nycflights13}
+
+## other compilations
+
+### {datasets}
+
+[The R datasets package](https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html)
+
+* Documentation for package ‘datasets’ version 3.7.0
+
+
+### {dslabs}
+
+Rafael Irizarry, 2018-01-22, [Some datasets for teaching data science](https://simplystatistics.org/2018/01/22/the-dslabs-package-provides-datasets-for-teaching-data-science/)
+
+### {fivethirtyeight}
+
+[fivethirtyeight Package](https://cran.r-project.org/web/packages/fivethirtyeight/vignettes/fivethirtyeight.html)
+
+
+### {untidy}
+
+[Untidy data sets for teaching R](https://community.rstudio.com/t/untidy-datasets-for-teaching-r/8008)
+
+***
+  
+[Creating R data packages for teaching](https://teachdatascience.com/datapackage/)
+
+Hadley Wickham, ["External data"](http://r-pkgs.had.co.nz/data.html), chapter from _R Packages_
+
+
+[Using Data Packages with R](https://okfnlabs.org/blog/2016/07/14/using-data-packages-with-r.html)
+
+
+[How to create an R data package](https://www.erikhoward.net/how-to-create-an-r-data-package/)
