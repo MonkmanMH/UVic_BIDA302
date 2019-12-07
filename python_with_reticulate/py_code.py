b463e0d609f4a105d6bdea954a3157ef12938cf2
Martin Monkman <monkmanmh@yahoo.ca>
2019-12-07 06:49:09 -0800

    hmm I think this is it

--- python_with_reticulate/py_code.py
+++ python_with_reticulate/py_code.py
@@ -1,7 +1,7 @@
 # this tests the import of pandas
 
-import pandas
-flights = pandas.read_csv("flights.csv")
+import pandas as pd
+flights = pd.read_csv("flights.csv")
 flights_ORD = flights[flights['dest'] == "ORD"]
 flights_ORD = flights_ORD[['carrier', 'dep_delay', 'arr_delay']]
 flights_ORD = flights_ORD.dropna()
