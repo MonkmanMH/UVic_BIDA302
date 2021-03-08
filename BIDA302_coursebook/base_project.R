3913edfdb5e219db7315dab9d4ddffda51a9d37c
Martin Monkman <monkmanmh@yahoo.ca>
2021-03-07 17:24:06 -0800

    the big move, part 1(?)

---
+++ BIDA302_coursebook/base_project.R
@@ -0,0 +1,16 @@
+## BIDA302 
+##
+## rstudio.cloud base project
+
+# tidyverse: core and others
+install.packages("tidyverse")
+install.packages("glue")
+
+# data
+install.packages("gapminder")
+install.packages("nycflights13")
+
+
+# utilities
+install.packages("janitor")
+
